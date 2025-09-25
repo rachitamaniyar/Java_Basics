1957e96b5a3f88000829c6bd8e8e87bd9aff47bf
rachitamaniyar <rachitamaniyar@gmail.com>
2025-09-25 15:28:30 +0200

    Create first_program.java

---
+++ variables/first_program.java
@@ -0,0 +1,18 @@
+ public static void first_program() {
+        System.out.printf("Name: Rachita Maniyar \nWetter: sonnig \nKatze: Edu");
+
+        System.out.println("John Doe");
+        System.out.println("Blauer Himmel & Sonnig");
+        System.out.println("Schnurrli oder Pezi");
+
+        String tag = "Mittwoch";
+        System.out.printf("Heute ");
+
+        int zahl = 10;
+        double zahl2 = 10.3;
+        float zahl3 = 15;
+
+        System.out.println("zahl: " + zahl);
+        System.out.println("zahl2: " + zahl2);
+        System.out.println("zahl3: " + zahl3);
+    }
