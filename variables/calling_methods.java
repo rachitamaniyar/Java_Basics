3531d92ef0a44d2454dc6a214b4f4ca7868583ad
rachitamaniyar <rachitamaniyar@gmail.com>
2025-09-25 15:31:18 +0200

    Create calling_methods.java

---
+++ variables/calling_methods.java
@@ -0,0 +1,24 @@
+  public static void ZwischenUebung() {
+        System.out.println("1) Zwischenübung: Zuerst werde ich aufgerufen!");
+        MethodenGrundlagen();
+        System.out.println("3) Zwischenübung: nun werde ich aufgerufen!");
+        ErsteMethode();
+        ZweiteMethode();
+        Methode2();
+    }
+
+    public static void MethodenGrundlagen() {
+        System.out.println("2) MethodenGrundlagen(): dann werde ich aufgerufen");
+    }
+
+    public static void Methode2() {
+        System.out.println("4) Methode2: zum schluss werde ich noch aufgerufen");
+    }
+
+    public static void ErsteMethode() {
+        System.out.println("Name: HG");
+    }
+
+    public static void ZweiteMethode() {
+        System.out.println("Wetter: Sonnig");
+    }
